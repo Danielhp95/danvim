094c447be569ce8177734407909ffcc635a25b80
Daniel Hernandez <daniel.hernandez2@sony.com>
2025-09-01 06:02:18 -0400

    Markdown make tab take only 2 spaces

---
+++ lua/danvim/plugins/previewers.lua
@@ -0,0 +1,8 @@
+return {
+  'sylvanfranklin/omni-preview.nvim',
+  opts = {},
+  dependencies = {
+    { 'wallpants/github-preview.nvim', lazy = true },
+    { 'hat0uma/csvview.nvim', lazy = true },
+  },
+}
