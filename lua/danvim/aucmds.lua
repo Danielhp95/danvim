e01542c1662895fa0d396584d15f728d14d562eb
Daniel Hernandez <daniel.hernandez2@sony.com>
2025-09-01 06:02:57 -0400

    Markdown make tab take only 2 spaes

--- lua/danvim/aucmds.lua
+++ lua/danvim/aucmds.lua
@@ -1,10 +1,20 @@
 -- [[ Highlight on yank ]]: Flashed text after being yanked
 -- See `:help vim.hl.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.hl.on_yank()
-	end,
-	group = highlight_group,
-	pattern = "*",
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.hl.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- Sets tab to 2 spaces on markdown files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
 })
