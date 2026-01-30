2a4e6e0a983246390bcb1ec8266f9989900093c6
Daniel Hernandez <daniel.hernandez2@sony.com>
2026-01-30 10:22:07 -0500

    added opencode

--- lua/danvim/plugins/avante.lua
+++ lua/danvim/plugins/avante.lua
@@ -1,52 +1,85 @@
 -- TODO: ADD BLINK compatibility
 return {
-  'yetone/avante.nvim',
-  event = 'VeryLazy',
-  opts = {
-    provider = 'ollama',
-    providers = {
-      ollama = {
-        -- model = 'phi4:14b',
-        model = 'copilot/claude-sonnet-4',
-      },
-    };
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  -- build = 'make',
-  build = 'make BUILD_FROM_SOURCE=true',
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	opts = {
+		provider = "copilot",
+		providers = {
+			ollama = {
+				model = "qwen3-coder:30b",
+				is_env_set = require("avante.providers.ollama").check_endpoint_alive,
+			},
+			-- copilot = {
+			-- 	endpoint = "https://api.githubcopilot.com",
+			-- 	model = "gpt-4o-2024-05-13",
+			-- 	proxy = nil, -- [protocol://]host[:port] Use this proxy
+			-- 	allow_insecure = false, -- Allow insecure server connections
+			-- 	timeout = 30000, -- Timeout in milliseconds
+			-- 	temperature = 0,
+			-- },
+		},
+	},
+	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	-- build = 'make',
+	build = "make BUILD_FROM_SOURCE=true",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		--- The below dependencies are optional,
+		"echasnovski/mini.pick", -- for file_selector provider mini.pick
+		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+		"ibhagwan/fzf-lua", -- for file_selector provider fzf
+		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+		"zbirenbaum/copilot.lua", -- for providers='copilot'
+		{
+			-- support for image pasting
+			"HakonHarnes/img-clip.nvim",
+			event = "VeryLazy",
+			opts = {
+				-- recommended settings
+				default = {
+					embed_image_as_base64 = false,
+					prompt_for_file_name = false,
+					drag_and_drop = {
+						insert_mode = true,
+					},
+				},
+			},
+		},
+	},
+	{
+  "NickvanDyke/opencode.nvim",
   dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'stevearc/dressing.nvim',
-    'nvim-lua/plenary.nvim',
-    'MunifTanjim/nui.nvim',
-    --- The below dependencies are optional,
-    'echasnovski/mini.pick', -- for file_selector provider mini.pick
-    'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
-    'ibhagwan/fzf-lua', -- for file_selector provider fzf
-    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-    'zbirenbaum/copilot.lua', -- for providers='copilot'
-    {
-      -- support for image pasting
-      'HakonHarnes/img-clip.nvim',
-      event = 'VeryLazy',
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-        },
-        rag_service = {
-          enabled = true, -- Enables the RAG service
-          host_mount = os.getenv 'HOME', -- Host mount path for the rag service
-          provider = 'ollama', -- The provider to use for RAG service (e.g. openai or ollama)
-          llm_model = 'phi4:14b', -- The LLM model to use for RAG service
-          embed_model = 'omic-embed-text', -- The embedding model to use for RAG service. (default)
-          endpoint = 'http://localhost:11434', -- The API endpoint for RAG service
-        },
-      },
-    },
+    -- Recommended for `ask()` and `select()`.
+    -- Required for `snacks` provider.
+    ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
+    { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
   },
+  config = function()
+    ---@type opencode.Opts
+    vim.g.opencode_opts = {
+      -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on the type or field.
+    }
+
+    -- Required for `opts.events.reload`.
+    vim.o.autoread = true
+
+    -- Recommended/example keymaps.
+    vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
+    vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
+    vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+
+    vim.keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
+    vim.keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
+
+    vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "Scroll opencode up" })
+    vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "Scroll opencode down" })
+
+    -- You may want these if you stick with the opinionated "<C-a>" and "<C-x>" above — otherwise consider "<leader>o…".
+    vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
+    vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
+  end,
+}
 }
