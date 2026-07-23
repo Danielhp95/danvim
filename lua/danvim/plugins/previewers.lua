51653cdd730463aa88fa09d0ac237fd689aae9dd
Daniel Hernandez <daniel.hernandez2@sony.com>
2026-07-23 16:24:41 +0200

    remove omni-preview and profile.nvim specs

--- lua/danvim/plugins/previewers.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'sylvanfranklin/omni-preview.nvim',
-  opts = {},
-  dependencies = {
-    { 'wallpants/github-preview.nvim', lazy = true },
-    { 'hat0uma/csvview.nvim', lazy = true },
-  },
-}
